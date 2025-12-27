7cb456090c448fe0a89f8f103a7664a2b55eb43d
Jonas Hellgren <jonas.hellgren@ri.se>
2025-12-27 10:00:18 +0100

    fixa pom

---
+++ src/test/java/setup/TestJupiterTest.java
@@ -0,0 +1,14 @@
+package setup;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestJupiterTest {
+
+
+     @Test
+      void whenTrue_thenTrue() {
+       Assertions.assertTrue(true);
+      }
+
+}
